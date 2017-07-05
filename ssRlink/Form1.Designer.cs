aca47a2a0ea3c01b4e9e10b1f651826aa91e7c56
yh-shadows <934405375@qq.com>
2017-07-05 18:49:03 +0800

    初版

---
+++ ssRlink/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ssRlink
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "转换";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // rtb
+            // 
+            this.rtb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb.Location = new System.Drawing.Point(12, 41);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(479, 213);
+            this.rtb.TabIndex = 3;
+            this.rtb.Text = "";
+            this.rtb.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 266);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.RichTextBox rtb;
+    }
+}
+
---
+++ 更新链接转换器/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 更新链接转换器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = web("https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/free/freenodeplain.txt");
+            //https://raw.githubusercontent.com/breakwa11/breakwa11.github.io/master/free/freenodeplain.txt
+            //if (str.IndexOf("MAX") < 5 && str.IndexOf("MAX") != -1)
+            str = Encoding.UTF8.GetString(Convert.FromBase64String(str));
+            string a = str.Replace("MAX=5", "MAX=99");
+            str = Convert.ToBase64String(Encoding.UTF8.GetBytes(a));
+
+            //else
+            //{
+
+            //}
+
+
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("C:\\wamp64\\www\\updata\\updata.txt", false))
+            {
+                sw.WriteLine(str);
+            }
+        }
+
+        public static string web(string web_src, string encod = "UTF8")
+        {
+            try
+            {
+
+                WebClient MyWebClient = new WebClient()
+                {
+                    Credentials = CredentialCache.DefaultCredentials//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                };
+                Byte[] pageData = MyWebClient.DownloadData(web_src); //从指定网站下载数据
+                string pageHtml = "";
+                if (encod == "gbk")
+                {
+                    pageHtml = Encoding.Default.GetString(pageData);  //如果获取网站页面采用的是GB2312，则使用这句            
+                }
+                else
+                {
+                    pageHtml = Encoding.UTF8.GetString(pageData); //如果获取网站页面采用的是UTF-8，则使用这句
+                }
+                return pageHtml;
+            }
+
+            catch (WebException webEx)
+            {
+
+                Console.WriteLine("网址访问出现以下错误:" + webEx.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
